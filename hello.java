083dfc218d9feb36bbe7dfcf760cd5ac5b7e87aa
zc <2165046074@qq.com>
2022-04-22 19:29:49 +0800

    my first commit

---
+++ hello.java
@@ -0,0 +1,8 @@
+public static void main(String [] args){
+System.out.println(123);
+System.out.println(123);
+System.out.println(123);
+System.out.println(1234);
+System.out.println(123);
+System.out.println(123);
+}
